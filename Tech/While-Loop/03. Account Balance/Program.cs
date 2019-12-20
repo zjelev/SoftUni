26a5d6b7d964b6f229d384dd9bce0f1424e848dc
zjelev <zlatomir@gmail.com>
2019-12-20 13:57:03 +0200

    Add files via upload

---
+++ Tech/While-Loop/03. Account Balance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03.Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int depositCount = int.Parse(Console.ReadLine());
+            double balance = 0;
+            while (depositCount > 0)
+            {
+                double deposit = double.Parse(Console.ReadLine());
+                if (deposit > 0)
+                {
+                    Console.WriteLine($"Increase: {deposit:f2}");
+                    balance += deposit;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                depositCount--;
+            }
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
