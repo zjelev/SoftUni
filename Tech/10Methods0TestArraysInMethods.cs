13e4674b211d0514b8084dedcba3c5e8e4e31a8b
zjelev <zlatomir@gmail.com>
2019-12-20 13:33:05 +0200

    Add files via upload

---
+++ Tech/10Methods0TestArraysInMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestArraysInMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            TestArr(arr);
+            Console.WriteLine(arr[0]);
+        }
+
+        static void TestArr(int[] arr1)
+        {
+            arr1[0] = arr1[3];
+        }
+    }
+}
