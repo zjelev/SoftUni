94e5e6018df0c914abf6aa502eda5b290a82d2d5
zjelev <zlatomir@gmail.com>
2019-12-20 13:49:08 +0200

    Add files via upload

---
+++ Tech/6.1. Drawing on the Console/07. Christmas Tree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 0; row <= n; row++)
+            {
+                for (int col = 0; col < n - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" | ");
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Tech/6.1. Drawing on the Console/09. House/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09.House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int stars = 1;
+            if (n%2==0)
+            {
+                stars++;
+            }
+            int roofLength = (int)Math.Ceiling(n / 2f);
+            for (int i = 0; i < roofLength; i++)
+            {
+                int padding = (n - stars) / 2;
+                var line = new string('-', padding)
+                    + new string('-', stars)
+                    + new string('-', padding);
+                Console.WriteLine(line);
+                stars += 2;
+            }
+            for (int i = 0; i < n / 2; i++)
+            {
+                var line = "|" + new string('*', n - 2) + "|";
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
