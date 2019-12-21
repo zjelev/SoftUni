26860eb84747da85a0b194d58a71e591b35695bc
zjelev <zlatomir@gmail.com>
2019-12-21 21:58:54 +0200

    Move SoftUni submissions

---
+++ C-Basics/Basics Exam - 6-7.04.2019/01. Oscars ceremony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.Oscars_ceremony
+{
+    class OscarsCeremony
+    {
+        static void Main(string[] args)
+        {
+            ushort rent = ushort.Parse(Console.ReadLine());
+            double statues = rent * 0.7;
+            double cathering = statues * 0.85;
+            double sound = cathering / 2;
+            string output = string.Format($"{rent + statues + cathering + sound:f2}");
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ C-Basics/Basics Exam - 6-7.04.2019/03. Oscars week in cinema/Program.cs
@@ -0,0 +1,54 @@
+﻿//https://judge.softuni.bg/Contests/Practice/Index/1596#2
+
+using System;
+
+namespace Scholarship
+{
+    class OscarsWeekInCinema
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
+
+            double gradeScholarship = Math.Floor(averageGrade * 25);
+            double socialScholarship = Math.Floor(minSalary * 0.35);
+
+            if (averageGrade < 4.5)
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+            else if (averageGrade < 5.5)
+            {
+                if (income < minSalary)
+                {
+                    Console.WriteLine($"You get a Social scholarship {socialScholarship} BGN");
+                }
+                else
+                {
+                    Console.WriteLine("You cannot get a scholarship!");
+                }
+            }
+            else
+            {
+                if (income >= minSalary)
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {gradeScholarship} BGN");
+                }
+                else
+                {
+                    if (gradeScholarship <= socialScholarship)
+                    {
+                        Console.WriteLine($"You get a Social scholarship {socialScholarship} BGN");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You get a scholarship for excellent results {gradeScholarship} BGN");
+                    }
+                }
+
+            } 
+        }
+    }
+}
