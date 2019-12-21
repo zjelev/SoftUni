5437f3cfb71257b7cce7558917187ac41555efff
zjelev <zlatomir@gmail.com>
2019-12-21 15:44:19 +0200

    moved from tech-csharp

---
+++ C-Basics/6.1. Drawing on the Console/02. Butterfly/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Butterfly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n-2; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine("{0}\\ /{0}", (new string('*', n - 2)));
+                else
+                {
+                    Console.WriteLine("{0}\\ /{0}", (new string('-', n - 2)));
+                }
+            }
+            Console.WriteLine("{0}@{0}", new string(' ', n - 1));
+            for (int i = 1; i <= n - 2; i++)
+            {
+                if (i%2 == 1)
+                Console.WriteLine("{0}/ \\{0}", (new string('*', n - 2)));
+                else
+                {
+                    Console.WriteLine("{0}/ \\{0}", (new string('-', n - 2)));
+                }
+            }
+        }
+    }
+}
---
+++ C-Basics/6.1. Drawing on the Console/03. Stop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.Stop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //top line
+            Console.WriteLine("{0}{1}{0}", new string('.', n + 1), new string('_', n + 4));
+            //upper body
+            int dots = n;
+            int lines = n + 2;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}//{1}\\\\{0}", new string('.', dots), new string('_', lines));
+                dots--;
+                lines = lines + 2;
+            }
+            //STOP line
+            Console.WriteLine("//{0}STOP!{0}\\\\", new string('_', (lines - 4) / 2));
+            //bottom
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}\\\\{1}//{0}", new string('.', dots), new string('_', lines));
+                dots++;
+                lines = lines - 2;
+            }
+        }
+    }
+}
