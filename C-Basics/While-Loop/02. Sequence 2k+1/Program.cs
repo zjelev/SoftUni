300c84c80cb48e0e99404cd51bb929ebd462bb92
zjelev <zlatomir@gmail.com>
2019-12-21 15:37:53 +0200

    Moved folder from Tech-CSharp

---
+++ C-Basics/While-Loop/02. Sequence 2k+1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter <= n)
+            {
+                Console.WriteLine(counter);
+                counter = counter * 2 + 1; 
+            } 
+        }
+    }
+}
