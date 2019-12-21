184fb62889129754570e2f9161c5e48e7de22d7d
zjelev <zlatomir@gmail.com>
2019-12-21 20:28:14 +0200

    Move my SoftUni submissions

---
+++ Tech/Associative Arrays/Exercise/05SoftUniParking/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var driversCars = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string command = input[0];
+                string user = input[1];
+
+                if (command == "register")
+                {
+                    string plateNum = input[2];
+                    if (!driversCars.ContainsKey(user))
+                    {
+                        driversCars.Add(user, plateNum);
+                        Console.WriteLine($"{user} registered {plateNum} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {driversCars[user]}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (driversCars.ContainsKey(user))
+                    {
+                        driversCars.Remove(user);
+                        Console.WriteLine($"{user} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {user} not found");
+                    }
+                }
+            }
+            
+            Console.WriteLine(string.Join(Environment.NewLine, driversCars
+            .Select(x => $"{x.Key} => {x.Value}")));
+            
+        }
+    }
+}
---
+++ Tech/Associative Arrays/Lab/02OddOccurrences/02OddOccurrences.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+                if (!synonyms.ContainsKey(word))
+                {
+                    synonyms[word] = new List<string>();
+                }
+                
+                synonyms[word].Add(synonim);
+            }
+            foreach (var item in synonyms)
+            {
+                var synonymsOfWord = item.Value;
+
+                System.Console.WriteLine($"{item.Key} - {string.Join(", ", synonymsOfWord)}");
+            }
+        }
+    }
+}
