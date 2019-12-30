aa947007e959d47edd84ffc3d1f0ca1b32780221
zjelev <zlatomir@gmail.com>
2019-12-30 15:18:45 +0200

    Add files via upload

---
+++ Tech/Exams/10.03.2019Group2/2.Seize the Fire/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.Seize_the_Fire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
