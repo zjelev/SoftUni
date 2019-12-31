ea745387941b2e1e85d7907f33feef91c24f4088
zjelev <zlatomir@gmail.com>
2019-12-31 11:00:57 +0200

    Add files via upload

---
+++ Tech/Data Types and Variables/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04._Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger cnt = BigInteger.Parse(Console.ReadLine());
+            BigInteger days = cnt * 3652422 / 100;
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", cnt, 
+                cnt * 100, days, days * 24, days * 24 * 60);
+        }
+    }
+}
---
+++ Tech/Data Types and Variables/I. Integer and Real Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I._Integer_and_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
