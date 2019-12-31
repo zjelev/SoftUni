a75099e83978ad774fb445ff477406c2cf093d08
zjelev <zlatomir@gmail.com>
2019-12-31 10:52:40 +0200

    Add files via upload

---
+++ C-Basics/Unclassified_Basics-05.2019/01. Numbers Ending in 7/Odd-Even/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _01._Numbers_Ending_in_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double oddSum = 0;
+            double oddMin = double.MaxValue;
+            double oddMax = double.MinValue;
+
+            double evenSum = 0;
+            double evenMin = double.MaxValue;
+            double evenMax = double.MinValue;
+
+            bool hasOdd = false;
+            bool hasEven = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double current = double.Parse(Console.ReadLine());
+
+                if(i%2 == 0)
+                {
+                    hasEven = true;
+                    if (current < evenMin)
+                    {
+                        evenMin = current;
+                    }
+
+                    if (current > evenMax)
+                    {
+                        evenMax = current;
+                    }
+
+                    evenSum += current;
+                }
+                else
+                {
+                    hasOdd = true;
+                    if (current < oddMin)
+                    {
+                        oddMin = current;
+                    }
+
+                    if (current > oddMax)
+                    {
+                        oddMax = current;
+                    }
+
+                    oddSum += current;
+                }
+            }
+
+            Console.WriteLine($"OddSum={oddSum:f2},");
+            if (hasOdd)
+            {
+                Console.WriteLine($"OddMin={oddMin:f2},");
+                Console.WriteLine($"OddMax={oddMax:f2},");
+            }
+            else
+            {
+                Console.WriteLine("OddMin=No,");
+                Console.WriteLine("OddMax=No,");
+            }
+
+            Console.WriteLine($"EvenSum={evenSum:f2},");
+            if (hasEven)
+            {
+                Console.WriteLine($"EvenMin={evenMin:f2},");
+                Console.WriteLine($"EvenMax={evenMax:f2}");
+            }
+            else
+            {
+                Console.WriteLine("EvenMin=No,");
+                Console.WriteLine("EvenMax=No");
+            }
+        }
+    }
+}
---
+++ C-Basics/Unclassified_Basics-05.2019/06. Cake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+
+            int totalPieces = width * lenght;
+
+            string input = Console.ReadLine();
+
+            while (input != "STOP")
+            {
+                totalPieces -= int.Parse(input);
+                if (totalPieces < 0)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+
+            if (input == "STOP")
+            {
+                Console.WriteLine($"{Math.Abs(totalPieces)} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(totalPieces)} pieces more.");
+            }
+
+        }
+    }
+}
