119701d6c182c68e19bba280b3ddb5da7b2a102d
zjelev <zlatomir@gmail.com>
2019-12-31 15:51:51 +0200

    Add files via upload

---
+++ Tech/Lists/MoreExercise/02. Car Race/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(" ")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            List<char> text = Console.ReadLine().ToList();
+
+            string newText = String.Empty;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                int currentNum = nums[i];
+                int sum = 0;
+
+                while (currentNum > 0)
+                {
+                    int currentDigit = currentNum % 10;
+                    sum += currentDigit;
+                    currentNum /= 10;
+                }
+
+                int elementIndex = sum % text.Count;
+
+                newText += text[elementIndex];
+
+                text.RemoveAt(elementIndex);
+            }
+
+            Console.WriteLine(newText);
+        }
+    }
+}
