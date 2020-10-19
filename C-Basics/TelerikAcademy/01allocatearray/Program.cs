dc8c66c92fb2826830f22d621a2bc09623cf2d4f
zjelev <zlatomir@gmail.com>
2020-10-19 14:07:40 +0300

    Add files via upload

---
+++ C-Basics/TelerikAcademy/01allocatearray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01allocatearray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = i * 5;
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
