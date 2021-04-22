33de5fd0429adfbafc9c7bcef4b0363196a5d535
zjelev <zlatomir@gmail.com>
2021-04-22 13:30:43 +0300

    Add files via upload

---
+++ C# OOP/04Polymorphism/2Animals/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // IAnimal animal = new Person();
+            // Mammal mammal = new Person();
+            // Person person = new Person();
+            
+            // ((Person)animal).Name = "Pesho";
+
+            Animal cat = new Cat("Pesho", "Whiskas");
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
