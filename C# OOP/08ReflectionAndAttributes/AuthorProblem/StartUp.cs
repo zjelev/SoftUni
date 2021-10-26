e5b2f92cd109a5d3af10d55ea72dca0ad8a15504
zjelev <zlatomir@gmail.com>
2021-10-26 17:58:14 +0300

    123

--- C# OOP/08ReflectionAndAttributes/AuthorProblem/StartUp.cs
+++ C# OOP/08ReflectionAndAttributes/AuthorProblem/StartUp.cs
@@ -8,5 +8,16 @@ namespace AuthorProblem
         {
             Console.WriteLine("Hello World!");
         }
+
+        [Flags]
+        public enum FileAccess
+        {
+            Read = 1,
+            Write = 2,
+            ReadWrite = Read | Write
+        }
+
+        [DllImport("user32.dll", EntryPoint="MessageBox")]
+        public static extern int ShowMessageBox(int hWnd, string text, string caption, int type);
     }
 }
