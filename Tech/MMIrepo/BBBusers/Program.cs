d3aeda78acb6a583a0d4c1d6cfe229c24f78d295
zjelev <zlatomir@gmail.com>
2021-12-16 12:55:19 +0200

    Converting lists of users to docker commands

---
+++ Tech/MMIrepo/BBBusers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BBBusers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("ShkoloNameMail.txt"))
+            {
+                using (var writer = new StreamWriter("Output.txt"))
+                {        
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    string line = reader.ReadLine();
+                    int allUsers = 0;
+                    int usersWithEmailsMMI = 0;
+                    while (line != null)
+                    {
+                        allUsers++;
+                        if (line.Contains("marica-iztok.com"))
+                        {
+                            // string pattern = @"[\w-]+@([\w-]+\.)+[\w-]+";
+                            // Regex regex = new Regex(pattern);
+                            // string replacement = @".";
+                            // newLine = regex.Replace(pattern, replacement);
+                            
+                            string[] lineArr = line.Split("\t", StringSplitOptions.RemoveEmptyEntries);
+                            lineArr[lineArr.Length - 2] = lineArr[lineArr.Length - 2] + "\",\"" + lineArr[lineArr.Length - 1];
+                            lineArr[lineArr.Length - 1] = String.Empty;
+                            string newLine = string.Join(" ", lineArr).Trim();
+                            newLine = string.Concat("docker exec greenlight-v2 bundle exec rake user:create[\"", newLine, "\",\"123456\",\"user\"]");
+                            writer.WriteLine(newLine);
+                            usersWithEmailsMMI++;                        
+                        }
+                        line = reader.ReadLine();
+                    }
+                    sw.Stop();
+                    Console.WriteLine($"From {allUsers} users with e-mails {usersWithEmailsMMI} have e-mails ending in marica-iztok.com.");
+                    Console.WriteLine($"Conversion took {sw.ElapsedTicks} timer ticks.");
+                }
+            }
+        }
+    }
+}
