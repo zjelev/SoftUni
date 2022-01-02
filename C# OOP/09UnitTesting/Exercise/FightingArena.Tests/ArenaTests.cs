a9ba34ec974788ae02a59136483da5b2c3645843
zjelev <zlatomir@gmail.com>
2022-01-02 20:01:29 +0200

    Finished Unit Testing Exercises

--- C# OOP/09UnitTesting/Exercise/FightingArena.Tests/ArenaTests.cs
+++ C# OOP/09UnitTesting/Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,48 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using FightingArena;
+using NUnit.Framework;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        Arena arena;
         [SetUp]
         public void Setup()
         {
+            arena = new Arena();
+
+            for (int i = 1; i < 4; i++)
+            {
+                Warrior warrior = new Warrior($"Warrior {i}", i * 10, i * 40);
+                arena.Enroll(warrior);
+            }
         }
 
         [Test]
-        public void Test1()
+        public void EnrollExistingNameThrowsExc()
         {
-            Assert.Pass();
+            Assert.Throws<InvalidOperationException>(()
+                => arena.Enroll(new Warrior("Warrior 2", 50, 100)));
+        }
+
+        [Test]
+        public void MissingWarriorsThrowExc()
+        {
+            Assert.Throws<InvalidOperationException>(()
+                => arena.Fight("Warrior 1", "Warrior 5"));
+            
+            Assert.Throws<InvalidOperationException>(()
+                => arena.Fight("Warrior 6", "Warrior 2"));
+        }
+
+        [Test]
+        public void ExistingWarriorsFight()
+        {
+            arena.Fight("Warrior 3", "Warrior 1");
+            Assert.AreEqual(10, arena.Warriors.FirstOrDefault(w
+                => w.Name == "Warrior 1").HP);
         }
     }
 }
--- C# OOP/09UnitTesting/Exercise/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/09UnitTesting/Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,66 @@
+using System;
+using FightingArena;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        Warrior warrior;
         [SetUp]
         public void Setup()
         {
+            warrior = new Warrior("name", 10, 20);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(null)]
+        [TestCase("  ")]
+        [TestCase("")]
+        public void CtorNameNullOrWhiteSpaceThrowsExc(string name)
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(()
+                => new Warrior(name, 10, 20));
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void CtorDamageZeroOrNegativeThrowsExc(int damage)
+        {
+            Assert.Throws<ArgumentException>(()
+                => new Warrior("name", damage, 20));
+        }
+
+        [TestCase(-1)]
+        public void CtorHPNegativeThrowsExc(int hp)
+        {
+            Assert.Throws<ArgumentException>(()
+                => new Warrior("name", 10, hp));
+        }
+
+        [TestCase(40)]
+        public void AttackBelowMinAttackHPThrowExc(int hp)
+        {
+            Assert.Throws<InvalidOperationException>(()
+                => warrior.Attack(new Warrior("name", 10, hp)));
+
+            warrior = new Warrior("name", 10, hp);
+
+            Assert.Throws<InvalidOperationException>(()
+                => warrior.Attack(new Warrior("name", 10, 20)));
+
+            Assert.Throws<InvalidOperationException>(()
+                => warrior.Attack(new Warrior("name", 50, 50)));
+        }
+
+        [TestCase(40)]
+        public void AttackReducesTargetsHPWithAttackersDamage(int damage)
+        {
+            warrior = new Warrior("name", damage, 100);
+            Warrior target = new Warrior("target", 5, 75);
+            warrior.Attack(target);
+            Assert.AreEqual(35, target.HP);
+            warrior.Attack(target);
+            Assert.AreEqual(0, target.HP);
         }
     }
 }
