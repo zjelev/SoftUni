83eaf68a7cfb262f766d0a473ad665c1dae4421c
zjelev <zlatomir@gmail.com>
2022-01-06 18:39:24 +0200

    moq

---
+++ C# OOP/09UnitTesting/MockingAndTDD/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class DummyTests
+{
+    private Dummy target;
+    private const int attackPoints = 10;
+    private const int health = 20;
+    private const int xp = 20;
+
+    [SetUp]
+    public void Initialize()
+    {
+        target = new Dummy(health, xp);
+    }
+
+    [Test]
+    public void LosesHealthIfAttacked()
+    {
+        target.TakeAttack(attackPoints);
+
+        Assert.That(target.Health, Is.EqualTo(10), "Dummy does not lose health on attack");
+    }
+
+    [Test]
+    public void DeadDummyAttacked()
+    {
+        target.TakeAttack(20);
+
+        Assert.That(() => target.TakeAttack(attackPoints),
+            Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."), 
+            "Dead dummy can be attacked");
+    }
+
+    [Test]
+    public void CanGiveXP()
+    {
+        target.TakeAttack(attackPoints * 2);
+        Assert.That(target.GiveExperience(), Is.EqualTo(20));
+    }
+
+    [Test]
+    public void CanNotGiveXP()
+    {
+         Assert.That(() => target.GiveExperience(),
+            Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+    }
+}
---
+++ C# OOP/09UnitTesting/MockingAndTDD/Skeleton.Tests/HeroTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class HeroTests
+{
+    [Test]
+    public void GainsXPWhenTargetDies()
+    {
+        //Arrange
+        Mock<IWeapon> mockedWeapon = new Mock<IWeapon>();
+        //mockedWeapon.Setup(w => w.AttackPoints).Returns(10);
+        //mockedWeapon.Setup(w => w.DurabilityPoints).Returns(10);
+        Mock<ITarget> mockedTarget = new Mock<ITarget>();
+        //mockedTarget.Setup(t => t.Health).Returns(0);
+        mockedTarget.Setup(t => t.IsDead()).Returns(true);
+        mockedTarget.Setup(t => t.GiveExperience()).Returns(10);
+        IWeapon weapon = mockedWeapon.Object;
+        ITarget target = mockedTarget.Object;
+        Hero hero = new Hero("Pesho", weapon);
+
+        //Act
+        hero.Attack(target);
+
+        Assert.That(hero.Experience, Is.EqualTo(10), "Hero does not gain XP when target dies");
+    }
+}
