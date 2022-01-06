35f2a6959073bfa385693013a220119855e7f809
zjelev <zlatomir@gmail.com>
2022-01-06 15:47:06 +0200

    Update Program.cs

--- C# EF Core/AdoNet/Program.cs
+++ C# EF Core/AdoNet/Program.cs
@@ -10,7 +10,6 @@ namespace AdoNet
         static void Main(string[] args)
         {
             string connString = @"Server = localhost,1433; Database = SoftUni; Integrated security = true;";
-            //string connString = @"Server = .; Database = D:\RETCOM\_DB\RK3\PERSONS.MDF; Integrated security = true;";
             int result;
             IList<Employee> employees = new List<Employee>();
 
@@ -21,7 +20,6 @@ namespace AdoNet
                 using (SqlCommand command = new SqlCommand())
                 {
                     command.CommandText = "SELECT TOP 10 * FROM Employees";
-                    //command.CommandText = "SELECT COUNT(*) FROM t_HoliDays";
                     command.Connection = dbCon;
 
                     result = (int)command.ExecuteScalar();
