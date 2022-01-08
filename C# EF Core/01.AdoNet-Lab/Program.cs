40a4abb20104c69d0b0489f8da4615940be93589
zjelev <zlatomir@gmail.com>
2022-01-08 09:20:45 +0200

    Changed AdoNet folder name

---
+++ C# EF Core/01.AdoNet-Lab/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AdoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = @"Server = .\SQLEXPRESS; Database = SoftUni; Integrated security = true;";
+            int result;
+            IList<Employee> employees = new List<Employee>();
+
+            using (SqlConnection dbCon = new SqlConnection(connString))
+            {
+                dbCon.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.CommandText = "SELECT TOP 10 * FROM Employees WHERE DepartmentId = @deptId";
+                    command.Connection = dbCon;
+                    // SqlParameter param = new SqlParameter();
+                    // param.DbType = System.Data.DbType.Int32;
+                    // param.ParameterName = "@deptId";
+                    // param.Value = 7;
+
+                    command.Parameters.AddWithValue("@deptId", 7);
+
+                    result = (int)command.ExecuteScalar();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                         while (reader.Read())
+                         {
+                              employees.Add(new Employee() 
+                              {
+                                  FirstName = reader["FirstName"]?.ToString(),
+                                  MiddleName = reader["MiddleName"]?.ToString(),
+                                  LastName = reader[2]?.ToString(),
+
+                              });
+                         }
+                    }
+                }
+            }
+
+            Console.WriteLine($"There are {result} rows");
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"{item.FirstName} {item.MiddleName} {item.LastName}");
+            }
+        }
+    }
+}
--- C# EF Core/AdoNet/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace AdoNet
-{
-    public class Employee
-    {
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-    }
-}
