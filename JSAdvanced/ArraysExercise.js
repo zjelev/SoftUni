2a9de1066bbb45cee4b30c66cfc9fa63c8a32158
zjelev <zlatomir@gmail.com>
2022-01-25 16:00:30 +0200

    16:00

---
+++ JSAdvanced/ArraysExercise.js
@@ -0,0 +1,22 @@
+function solve(input) {
+    // input.forEach(element => {console.log(element);
+    // });
+
+    // for (const key in input) {
+    //     if (Object.hasOwnProperty.call(input, key)) {
+    //         const element = key;
+    //         console.log(element);
+    //     }
+    // }
+
+    // for (const iterator of input) {
+    //     console.log(iterator);
+    // }
+
+    console.log(input.reduce((acc, curr) => acc *= curr))
+}
+
+solve([1, 2 ,3, 4, 1, 9])
+
+// React!!!
+
