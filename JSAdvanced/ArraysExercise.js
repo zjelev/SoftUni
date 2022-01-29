4e653084a67348adc43b57c52a28c1f678c79f9b
zjelev <zlatomir@gmail.com>
2022-01-29 15:21:24 +0200

    TicTacToe ugly 100/100

--- JSAdvanced/ArraysExercise.js
+++ JSAdvanced/ArraysExercise.js
@@ -86,19 +86,83 @@
 //     });
 //     return input;
 // }
-// console.log(increasingSubsequence([0, 3, 2, 15, 6, 1]));
+// // console.log(increasingSubsequence([0, 3, 2, 15, 6, 1]));
 
-//6
-function arraySort2Crit(input) {
-    input.sort((a, b) => {
-        if (a.length > b.length) {
-            return 1;
-        } else if (a.length === b.length) {
-            return (a > b);
+// //6
+// function arraySort2Crit(input) {
+//     // input.sort((a, b) => {
+//     //     if (a.length > b.length) {
+//     //         return 1;
+//     //     } else if (a.length === b.length) {
+//     //         return (a > b);
+//     //     } else {
+//     //         return -1;
+//     //     }
+//     // });
+//     const twoCrtSort = (a, b) => a.length - b.length || a.localeCompare(b);
+//     input.sort(twoCrtSort);
+//     return input;
+// }
+// console.log(arraySort2Crit(['test', 'Deny', 'omen', 'Default']));
+
+// 8
+function tictactoe(moves) {
+    let matrix = [[false, false, false],
+                  [false, false, false],
+                  [false, false, false]];
+
+    let counter = 0;
+
+    for (const move of moves) {
+        let [x, y] = move.split(' ').map(num => Number(num));
+        if (matrix[x][y]) {
+            console.log(`This place is already taken. Please choose another!`);
         } else {
-            return -1;
+            if (counter % 2 === 0) {
+                matrix[x][y] = 'X'
+            } else {
+                matrix[x][y] = 'O'
+            }
+            counter++;
+
+            if (counter === matrix.length ** 2) {
+                console.log('The game ended! Nobody wins :(');
+                for (const row of matrix) {
+                    console.log(row.join('	'));
+                }
+                return;
+            }
+
+            // for (let i = 0; i < matrix.length; i++) {
+            //     if (matrix[i][i] === matrix[i][i + 1] && matrix[i][i + 1] === matrix[i][i + 2] && matrix[i][i] ||
+            //         matrix[i][i] === matrix[i + 1][i] && matrix[i + 1][i] === matrix[i + 2][i] && matrix[i][i]
+            //     ) {
+            //         console.log(`Player ${matrix[x][y]} wins`);
+            //         for (const row of matrix) {
+            //             console.log(row.join(' '));
+            //         }
+            //         return;
+            //     }
+            // }
+
+            if (matrix[0][0] === matrix[0][1] && matrix[0][1] === matrix[0][2] && matrix[0][0] ||
+                matrix[1][0] === matrix[1][1] && matrix[1][1] === matrix[1][2] && matrix[1][1] ||
+                matrix[2][0] === matrix[2][1] && matrix[2][1] === matrix[2][2] && matrix[2][2] ||
+                matrix[0][0] === matrix[1][0] && matrix[1][0] === matrix[2][0] && matrix[0][0] ||
+                matrix[0][1] === matrix[1][1] && matrix[1][1] === matrix[2][1] && matrix[1][1] ||
+                matrix[0][2] === matrix[1][2] && matrix[1][2] === matrix[2][2] && matrix[2][2] ||
+                matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2] && matrix[1][1] ||
+                matrix[2][0] === matrix[1][1] && matrix[1][1] === matrix[0][2] && matrix[1][1]) {
+                console.log(`Player ${matrix[x][y]} wins!`);
+                for (const row of matrix) {
+                    console.log(row.join('	'));
+                }
+                return;
+            }
         }
-    });
-    return input;
+    }
 }
-console.log(arraySort2Crit(['test', 'Deny', 'omen', 'Default']));
+
+// tictactoe(["0 1", "0 0", "0 2", "2 0", "1 0", "1 2", "1 1", "2 1", "2 2", "0 0"])
+// tictactoe(["0 0","0 0","1 1","0 1","1 2","0 2","2 2","1 2","2 2","2 1"])
+tictactoe(["0 1","0 0","0 2", "2 0","1 0","1 1","1 2","2 2","2 1","0 0"])
