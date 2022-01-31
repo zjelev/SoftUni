053c279483008a432e565fdef2d548a86a9d386f
zjelev <zlatomir@gmail.com>
2022-01-31 10:28:05 +0200

    task 1

--- JSAdvanced/ObjectsAndClasses.js
+++ JSAdvanced/ObjectsAndClasses.js
@@ -1,7 +1,9 @@
 let person = {
     firstName: 'Pesho',
     lastName: 'Petrov',
-    age: 30, 
+    age: function (myAge) {
+        return `My age is ${myAge}`;
+    },
     talk: function () {
         person.greet('qewwqdas');
     },
@@ -9,8 +11,64 @@ let person = {
     greet: name => console.log('Hello ' + name + '!'),
 }
 
-person.talk();
+// person.talk();
 
-let person2 = Object.assign({}, person);
-let propName = 'firstName';
-console.log(person2[propName]);
+// let person2 = Object.assign({}, person);
+// let propName = 'firstName';
+// console.log(person2[propName]);
+
+// person['jobtitle'] = 'Trainer';
+// console.log(person['jobtitle']);
+// console.log(person.age(30));
+// delete person.age;
+// console.log(person.age);
+
+// let keys = Object.keys(person);
+// //console.log(keys);
+// if (person.hasOwnProperty('age')) {
+//     console.log(person.age(30));  // JS Core 
+// }
+
+// let values = Object.values(person);
+// console.log(values);
+// if (values.includes('Pesho')) {
+//     console.log("found value");
+// }
+
+// for (const key in person) {
+//     console.log(`${key} --> ${person[key]}`);
+// }
+
+// for (const el of Object.keys(person)) {
+//      console.log(`${el} --> ${person[el]}`);
+// }
+
+// let data = '[{"manager": {"firstName":"Pesho","lastName":"Petrov","height":180}}, {"firstName": "Gosho"}]'; //JSON.stringify(person);
+// // console.log(data);
+// let person2 = JSON.parse(data);
+// console.log(person2[0].manager.firstName);
+
+//1
+function townsToJson(input) {
+    let data = input
+        .map(row => row.split('|').filter(x => x != '').map(x => x.trim()));
+
+    let properties = data.shift();
+
+    let result = [];
+
+    data.forEach(row => {
+        let town = {
+            [properties[0]]: row[0],
+            [properties[1]]: Number(Number(row[1]).toFixed(2)),
+            [properties[2]]: Number(Number(row[2]).toFixed(2)),
+        };
+
+        result.push(town);
+    });
+
+    console.log(JSON.stringify(result));
+}
+
+townsToJson(['| Town | Latitude | Longitude |','| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |'])
