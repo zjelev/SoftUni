956062b820721900ca413ac22180ba2a4ddae4d9
zjelev <zlatomir@gmail.com>
2022-02-07 15:01:45 +0200

    stuck in tack 4

--- JSAdvanced/Arrays.js
+++ JSAdvanced/Arrays.js
@@ -1,22 +1,19 @@
 // // Lab
 // // 05:00
 // console.log(calc(sum, 1, 1)); // Hoisting
+// let sum = (a, b) => a + b;   // Definition - Hoising does not work
+function sum(a, b) {        // Declaration - Hoisting is OK
+    return a + b;
+}
 
-// // let sum = (a, b) => a + b; // Definition - Hoising does not work
-
-// function sum(a, b) {        // Declaration - Hoisting is OK
-//     return a + b;
-// }
-
-// function calc(oper, arg1, arg2) {
-//     let result = oper(arg1, arg2);
-//     console.log(num);
-//     var num = 10;        //with var num is hoisted (hiddenly declared) in beginning of scope
-//     return result + num;
-// }
+function calc(oper, arg1, arg2) {
+    let result = oper(arg1, arg2);
+    console.log(num);
+    var num = 10;        //with var num is hoisted (hiddenly declared) in beginning of scope
+    return result + num;
+}
 
 // let sum = 5; // Error
-
 // let result = calc(sum, 5, 10);
 
 // //26:12
@@ -35,85 +32,77 @@
 // console.log(array); 
 // console.log(array.length);  //WTF
 
-// // 46:15
+// 46:15
 let names = ['Pesho', 'Gosho', 'ivan', 'Stamat', 'Batman', 'superman', 'SpiderMan', 'captain America', 'Wonder Woman'];
 
 // getNames(names);
+function getNames(names) {
+    // let firstName = names[0];
+    // let secondName = names[1];
+    // // Array destructuring
+    let [firstName, ...others] = names; // Rest operator
 
-// function getNames(names) {
-//     // let firstName = names[0];
-//     // let secondName = names[1];
-//     // // Array destructuring
-//     let [firstName, ...others] = names; // rest operator
-
-//     console.log(firstName);
-//     console.log(others);
-// }
+    console.log(firstName);
+    console.log(others);
+}
 
 // // 51:00 Mutator Methods
 // let lastName = names.pop();
-
 // names.push('Gerry');
 
-// function solve(...names) {           // Rest operator
-//     console.log(names.join(', '));
-// }
+function solve(...names) {           // Rest operator
+    console.log(names.join(', '));
+}
 // solve('pesho', 'gosho', 'stamat');
 
 // let numbers = [10, 2, 100, 4];
 // let largestNumber = Math.max(...numbers);   // Spread operator
 
-// // 1:14 - Task 1
-// function sumFirstLast(params) {
-//     console.log(Number(params[0]) + Number(params[params.length - 1]));
-// }
-
+// 1:14 - Task 1
+function sumFirstLast(params) {
+    console.log(Number(params[0]) + Number(params[params.length - 1]));
+}
 // sumFirstLast(['20', '30', '40']);
 // let result = names.splice(2, 1);
 
-// // 2
-// function evenPositionElement(params) {
-//     let output = '';
-//     // for (let i = 0; i < params.length; i+=2) {
-//     //     output += params[i] + ' ';
-//     // }
-
-//     for (const key in params) {
-//         if (key % 2 == 0) {
-//             output += params[key] + ' ';
-//         }
-//     }
-//     console.log(output);
-// }
-
+// 2
+function evenPositionElement(params) {
+    let output = '';
+    // for (let i = 0; i < params.length; i+=2) {
+    //     output += params[i] + ' ';
+    // }
+    for (const key in params) {
+        if (key % 2 == 0) {
+            output += params[key] + ' ';
+        }
+    }
+    console.log(output);
+}
 // evenPositionElement(['5', '30', '40']);
 
-// // 3
-// function negativePositiveNums(params) {
-//     let array = [];
-//     params.forEach(element => {
-//         if (element <  0) {
-//             array.unshift(element);
-//         } else {
-//             array.push(element)
-//         }
-//     });
-//     console.log(array.join(' '));
-// }
+// 3
+function negativePositiveNums(params) {
+    let array = [];
+    params.forEach(element => {
+        if (element < 0) {
+            array.unshift(element);
+        } else {
+            array.push(element)
+        }
+    });
+    console.log(array.join(' '));
+}
 
-// let result;
-
-// result = names.splice(2, names.length, 1, 3, 5); 
-
+// let result = names.splice(2, names.length, 1, 3, 5); 
 // let arr = [1, 2, 3, 4, 5, 6, 7, 10, 100, 256];
 // arr.length = 5;
 // arr.fill(5, 1, 3);  // Ctrl + Shift + Space
 // arr.reverse();
 
 // names.sort((a, b) => a.localeCompare(b));
-// function compareNums(a, b) {
-//     return a - b;
-// }
+function compareNums(a, b) {
+    return a - b;
+}
 // arr.sort();              //Sorts by Inicode symbol code
 // arr.sort((a, b) => b - a);  //Arrow function (lambda in c#)
 
@@ -154,33 +143,17 @@ let names = ['Pesho', 'Gosho', 'ivan', 'Stamat', 'Batman', 'superman', 'SpiderMa
 // let start;
 // let stop;
 
-// function lastKNumbersSequence(n, k) {
-//     let nums = [1];
-//     let sumPreviosElements = 0;
-//     for (let i = 0; i < k - 1; i++) {
-//         sumPreviosElements += nums[i];
-//         nums.push(sumPreviosElements);
-//     }
-    
-//     // for (let i = k; i < n; i++) {
-//     //     let nextNum = 0;
-//     //     for (let j = k; j > 0; j--) {
-//     //         nextNum += Number(nums[i - j]);
-//     //     }
-//     // }
-
-//     while(n-- > k) {
-//         let sum = 0;
-//         for (let i = nums.length - 1; i > nums.length - 1 - k; i--) {
-//             //if (typeof(nums[i]) == 'number') {
-//                 sum += Number(nums[i]);
-//             // }
-//         }
-//         nums.push(sum);
-//     }
-
-//     return nums;
-// }
+function lastKNumbersSequence(n, k) {
+    let nums = [1];
+    for (let i = 1; i < n; i++) {
+        let lastK = nums.slice(i - k, i);
+        let sum = 0;
+        lastK.forEach(x => sum += x);
+        nums[i] = sum;
+    }
+    return nums;
+}
+console.log(lastKNumbersSequence(8, 2))
 
 // function numSequent(n, k) {
 //     let output = [1];
@@ -206,84 +179,80 @@ let names = ['Pesho', 'Gosho', 'ivan', 'Stamat', 'Batman', 'superman', 'SpiderMa
 // stop = Date.now();
 // console.log(`Other code spent ${stop - start} ms`);
 
-// // 5
-// function processOddPositions(params) {
-//     let result = params.filter((v, i) => i % 2 == 1).map(v => v * 2).reverse();
-//     return result;
-// }
-
+// 5
+function processOddPositions(params) {
+    let result = params.filter((v, i) => i % 2 == 1).map(v => v * 2).reverse();
+    return result;
+}
 // console.log(processOddPositions([10, 15, 20, 25]))
 
-// // 6
-// function smallestTwoNumbers(params) {
-//     let result = params.sort((a, b) => a -b).slice(0, 2)
-//     return result;
-// }
+// 6
+function smallestTwoNumbers(params) {
+    let result = params.sort((a, b) => a -b).slice(0, 2)
+    return result;
+}
 
 // console.log(smallestTwoNumbers([30, 15, 50, 5]));
 
-// // 7
-// function biggestElement(matrix) {  
-//     //let maxNum = Number.MIN_SAFE_INTEGER;
-//     // matrix.forEach(row => {
-//     //     row.forEach(el => {
-//     //         if (el > maxNum) {
-//     //             maxNum = el;
-//     //         }
-//     //     })
-//     // });
+// 7
+function biggestElement(matrix) {  
+    //let maxNum = Number.MIN_SAFE_INTEGER;
+    // matrix.forEach(row => {
+    //     row.forEach(el => {
+    //         if (el > maxNum) {
+    //             maxNum = el;
+    //         }
+    //     })
+    // });
 
-//     // matrix.forEach(row => {
-//     //     let maxInRow = Math.max(...row);
-//     //     if ( maxInRow > maxNum) {
-//     //         maxNum = maxInRow;
-//     //     }
-//     // });
+    // matrix.forEach(row => {
+    //     let maxInRow = Math.max(...row);
+    //     if ( maxInRow > maxNum) {
+    //         maxNum = maxInRow;
+    //     }
+    // });
 
-//     // let maxNums = matrix.map(row => Math.max(...row));
-//     // let maxNum = Math.max(...maxNums);
+    // let maxNums = matrix.map(row => Math.max(...row));
+    // let maxNum = Math.max(...maxNums);
 
-//     let maxNum = matrix
-//         .map(row => Math.max(...row))
-//         .reduce((a, x) => Math.max(a, x), Number.MIN_SAFE_INTEGER)
+    let maxNum = matrix
+        .map(row => Math.max(...row))
+        .reduce((a, x) => Math.max(a, x), Number.MIN_SAFE_INTEGER)
 
-//     return maxNum;
-// }
-
+    return maxNum;
+}
 // console.log(biggestElement([[3, 5, 7, 12],[-1, 4, 33, 2],[8, 3, 0, 4]]));
 // console.log(biggestElement([[20, 50, 10], [8, 33, 145]]));
 
-// // 8 
-// function diagonalSums(matrix) {
-//     let leftRightSum = 0;
+// 8 
+function diagonalSums(matrix) {
+    let leftRightSum = 0;
 
-//     for (let i = 0; i < matrix.length; i++) {
-//         leftRightSum += matrix[i][i]; 
-//     }
+    for (let i = 0; i < matrix.length; i++) {
+        leftRightSum += matrix[i][i]; 
+    }
 
-//     let rightLeftSum = 0;
-//     let reverseCounter = matrix.length;
-//     for (let i = 0; i < matrix.length; i++) {
-//         rightLeftSum += matrix[i][--reverseCounter];
-//     }
-
-//     return leftRightSum + ' ' + rightLeftSum;
-// }
-
+    let rightLeftSum = 0;
+    let reverseCounter = matrix.length;
+    for (let i = 0; i < matrix.length; i++) {
+        rightLeftSum += matrix[i][--reverseCounter];
+    }
+    return leftRightSum + ' ' + rightLeftSum;
+}
 // console.log(diagonalSums([[3, 5, 17],[-1, 7, 14],[1, -8, 89]]));
 
 // 9. 
 function equalNeighbors(matrix) {
     let sum = 0;
     let lastRow = matrix[matrix.length - 1];
-    
+
     for (let i = 0; i < matrix.length - 1; i++) {
         for (let j = 0; j < matrix[i].length - 1; j++) {
             if ((matrix[i][j] === matrix[i + 1][j])) {
                 sum++;
             }
 
-            if (matrix[i][j] === matrix[i][j+1]) {
+            if (matrix[i][j] === matrix[i][j + 1]) {
                 sum++;
             }
         }
@@ -300,10 +269,8 @@ function equalNeighbors(matrix) {
             sum++
         }
     }
-
     return sum;
 }
-
 // console.log(equalNeighbors([['2', '3', '4', '7', '0'],
 // ['4', '0', '5', '3', '4'],
 // ['2', '3', '5', '4', '2'],
@@ -313,6 +280,6 @@ function equalNeighbors(matrix) {
 // ['well', 'done', 'yo', '6'],
 // ['not', 'done', 'yet', '5']]));
 
-console.log(equalNeighbors([[2, 2, 5, 7, 4],
-                            [4, 0, 5, 3, 4],
-                            [2, 5, 5, 4, 2]]))
+// console.log(equalNeighbors([[2, 2, 5, 7, 4],
+// [4, 0, 5, 3, 4],
+// [2, 5, 5, 4, 2]]))
