f17dbe1f7f105970bf7f2332670f282410fd3b05
zjelev <zlatomir@gmail.com>
2022-02-14 16:00:29 +0200

    Add files via upload

---
+++ C# EF Core/MiniORM/ChangeTracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
+{
+	internal class ChangeTracker<T>
+		where T: class, new()
+	{
+		private readonly List<T> allEntities;
+		private readonly List<T> added;
+		private readonly List<T> removed;
+
+		public ChangeTracker(IEnumerable<T> entities)
+		{
+			this.added = new List<T>();
+			this.removed = new List<T>();
+			this.allEntities = CloneEntities(entities);
+		}
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var clonedEntities = new List<T>();
+			var propertiesToClone = typeof(T).GetProperties()
+				.Where(pi => DbContext
+					.AllowedSqlTypes
+					.Contains(pi.GetType))
+				.ToArray();
+        }
+    }
+}
---
+++ C# EF Core/MiniORM/DbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniORM
+{
+	class DbContext
+	{
+
+	}
+}
