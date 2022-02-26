83a750c06d7032a627dd933fc2aac37cf79bfe54
zjelev <zlatomir@gmail.com>
2022-02-26 08:55:24 +0200

    Add files via upload

---
+++ C# EF Core/MiniORM.App/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace MiniORM.App // Note: actual namespace depends on the project name.
+{
+    using Data;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=.;database=MiniORM;Integrated Security=True";
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Ivan",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.LastName = "Ivanov";
+
+            context.Departments.Add(new Department
+            {
+                Name = "Sales"
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
