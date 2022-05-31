b651a21da2ba8efd49ff5302e2c77a1a83f2611a
zjelev <zlatomir@gmail.com>
2022-05-31 10:25:17 +0300

    Add files via upload

---
+++ C# Advanced/02JaggedArrays/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            
+            // 6. Jagged-Array Modification
+            /*
+            int[][] jagged = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                jagged[i] = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            string[] input = Console.ReadLine()
+                    .Split(' ');
+
+            while (input[0] != "END")
+            {
+                string command = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+                int operand = int.Parse(input[3]);
+
+                if (row < 0 || row >= rows || col < 0 || col >= jagged[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if (command.ToLower() == "add")
+                    {
+                        jagged[row][col] += operand;
+                    }
+
+                    if (command.ToLower() == "subtract" )
+                    {
+                        jagged[row][col] -= operand;
+                    }
+                }
+                input = Console.ReadLine()
+                    .Split(' ');
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write($"{jagged[row][col]} ");
+                }
+
+                Console.WriteLine();
+            }
+            */
+
+            //7.Pascal Triangle
+            long[][] jagged = new long[rows][];
+            int currentLenght = 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                jagged[i] = new long[currentLenght];
+                jagged[i][0] = 1;
+                jagged[i][currentLenght -1] = 1;
+
+                if (currentLenght > 2)
+                {
+                    for (int j = 1; j < currentLenght - 1; j++)
+                    {
+                        jagged[i][j] = jagged[i-1][j-1] + jagged[i-1][j];
+                    }
+                }
+
+                currentLenght++;
+            }
+
+            /*
+            if (rows > 0)
+            {                
+                jagged[0] = new int[1];
+                jagged[0][0] = 1;
+                for (int row = 2; row <= rows; row++)
+                {
+                    jagged[row-1] = new int[row];
+                    jagged[row-1][0] = 1;
+                    for (int col = 1; col < row - 2; col++)
+                    {
+                        jagged[row-1] = new int[row];
+                        jagged[row-1][col] = jagged[row - 2][col - 1] + jagged[row - 2][col];
+                    }
+                }
+            }
+            */
+
+           foreach (long[] row in jagged)
+            {
+               Console.WriteLine(String.Join(' ', row));
+            }
+        }
+    }
+}
---
+++ C# Advanced/02MultidimentionalArrays/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace MultidimentionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Sum Matrix Elements 
+            /*
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    //.Split(", ")  // 1. Sum Matrix Elements 
+                    .Split(" ")     //2. Sum Matrix Columns
+                    .Select(int.Parse)
+                    .ToArray();
+                
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sum = 0;
+
+            
+            Console.WriteLine(size[0]);
+            Console.WriteLine(size[1]);
+            
+
+            foreach (var item in matrix)
+            {
+                sum += item;    
+            }
+            Console.WriteLine(sum);
+            */
+
+            //2. Sum Matrix Columns
+            /*
+            for (int col = 0; col < size[1]; col++)
+            {
+                for (int row = 0; row < size[0]; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            */
+
+            //3.Primary Diagonal
+            /*
+            int size = int.Parse(Console.ReadLine());
+            int [,] matrix = new int[size, size];
+            
+            for (int row = 0; row < size; row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+            */
+
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            
+            for (int row = 0; row < size; row++)
+            {
+                string currentRow = Console.ReadLine();
+                
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] =  currentRow[col];
+                }
+            }
+            var symbol = Console.ReadLine();
+            bool found = false;
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == char.Parse(symbol))
+                    {
+                        found = true;
+                        Console.WriteLine($"({row}, {col})");
+                    }
+                }
+            }
+
+            if (found == false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
