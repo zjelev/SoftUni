68d1eadc7349f3c7d17d9176e9ec1fd38ae84624
zjelev <zlatomir@gmail.com>
2022-06-05 15:17:16 +0300

    2:07

--- C# EF Core/EFDemo/Program.cs
+++ C# EF Core/EFDemo/Program.cs
@@ -8,20 +8,36 @@ namespace EFDemo
         static void Main(string[] args)
         {
             // dotnet add package Microsoft.EntityFrameworkCore.SqlServer
-            // dotnet add package Microsoft.EntityFrameworkCore.SqlServer.Design
+            // dotnet add package Microsoft.EntityFrameworkCore.SqlServer.Design   // or Tools 
             // dotnet tool install --global dotnet-ef
 
-            // string ->  MAX, Unicode  DB -> char(50) => EF Fluent Api
-            // dotnet ef dbcontext scaffold "Server=.;Database=SoftUni;Integrated Security=true;" 
-                //Microsoft.EntityFrameworkCore.SqlServer -o Models -f --data-annotations
+            // string ->  MAX, Unicode  DB -> char(50)   => EF Fluent API
+            // dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Database=SuTest;Integrated Security=true;" Microsoft.EntityFrameworkCore.SqlServer -o Models -f --data-annotations
             
             // SQL Server Profiler -> Use the template = TSQL_Duration
 
             // Always use Select().ToList() - get from db only what you need!
 
-            var db = new SoftUniContext();
-            Console.WriteLine(db.Employees.Count(x => x.Projects.Count() > 2));
+            var db = new SuTestContext();
+            // Console.WriteLine(db.Employees.Count());
+            //Console.WriteLine(db.Employees.Count(x => x.Projects.Count() > 3));
+            // var employees = db.Employees.ToList(); //Don't do that, use Select before ToList
+            var employees = db.Employees.Select(x =>
+                new
+                {
+                    x.LastName,
+                    NumProjects = x.Projects.Count()
+                }).OrderByDescending(x => x.NumProjects).Take(50)
+                .ToList();
 
+            foreach (var empl in employees)
+            {
+                System.Console.WriteLine($"{empl.LastName} => {empl.NumProjects}");
+            }
+
+            int? a = null;
+            DateTime? d = DateTime.UtcNow;
+            d = null;
         }
     }
 }
