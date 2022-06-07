b6c09913baf2eafeecced578e03b6e176f1bb12f
zjelev <zlatomir@gmail.com>
2022-06-07 15:58:02 +0300

    Add files via upload

---
+++ C# EF Core/08.EFIntro/StartUp.cs
@@ -0,0 +1,130 @@
+﻿// dotnet add package Microsoft.EntityFrameworkCore.Tools -v 3.1.3
+// dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+// dotnet add package Microsoft.EntityFrameworkCore.SqlServer.Design
+// dotnet ef dbcontext scaffold "Server=.;Database=SoftUni;Integrated Security=True;" Microsoft.EntityFrameworkCore.SqlServer -o Data\Models
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = AddNewAddressToEmployee(context);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+            .Employees
+            .Select(e => new
+            {
+                e.EmployeeId,
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .OrderBy(e => e.EmployeeId)
+            .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name.Equals("Research and Development"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            Address address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee employee = context
+                .Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+            
+            context.Addresses.Add(address); // Can be omitted
+            employee.Address = address;
+
+            context.SaveChanges();
+
+            List<string> addresses = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address.AddressText)
+                .ToList();
+
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
