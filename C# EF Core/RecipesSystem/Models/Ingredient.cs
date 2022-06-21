679b35709309b6351edf8af44e133002654bb378
zjelev <zlatomir@gmail.com>
2022-06-21 16:09:57 +0300

    Entity Relations - Attributes
    2:55

--- C# EF Core/RecipesSystem/Models/Ingredient.cs
+++ C# EF Core/RecipesSystem/Models/Ingredient.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RecipesSystem.Models
 {
+    [Table("Ingredienti", Schema = "dbo")]
     public class Ingredient
     {
         public int Id { get; set; }
 
+        [Column("Title", Order = 3, TypeName = "nvarchar(90)")]
+        [Required]
         public string Name { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        // Has-A relation
+        public int RecipeId { get; set; } // not required, but good to have
+
+        public Recipe Recipe { get; set; }
     }
 }
--- C# EF Core/RecipesSystem/Models/Recipe.cs
+++ C# EF Core/RecipesSystem/Models/Recipe.cs
@@ -1,23 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RecipesSystem.Models
 {
     public class Recipe
     {
+        [Key]
         public int Id { get; set;  }
 
         [Required]
         [MaxLength(100)]
-
+        [MinLength(10)] // does not affect DB, as [Range]
         public string Name { get; set;  }
 
         public string Description { get; set; }
 
+        [MaxLength(80)]
+        [NotMapped]
         public string Test { get; set; }
 
-        public TimeSpan CookingTime { get; set; }
+        public TimeSpan? CookingTime { get; set; }
 
         public ICollection<Ingredient> Ingredients { get; set; }
 
