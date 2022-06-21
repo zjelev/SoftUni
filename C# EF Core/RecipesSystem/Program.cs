4ffff87209fef42ed1c7fb3cc4bc37a8644047e4
zjelev <zlatomir@gmail.com>
2022-06-21 14:44:39 +0300

    Entity Relations - Fluent Api
    2:20

--- C# EF Core/RecipesSystem/Program.cs
+++ C# EF Core/RecipesSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using RecipesSystem.Models;
 
@@ -9,11 +10,22 @@ namespace RecipesSystem
         static void Main(string[] args)
         {
             var db = new RecipesDbContext();
-            // db.Database.EnsureDeleted();
-            // db.Database.EnsureCreated();
-            db.Database.Migrate(); // dotnet ef database update
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            // db.Database.Migrate(); // dotnet ef database update
 
-            db.Recipes.Add(new Recipe { Name = "Musaka" });
+            db.Recipes.Add(new Recipe 
+            { 
+                Name = "Musaka 4",
+                Description = "Traditional BG/Tr meal",
+                CookingTime = new TimeSpan(2, 3, 4) 
+            });
+
+            db.Recipes.Select(x => new
+            {
+                Egn = EF.Property<string>(x, "Egn") // shadow property
+            });
+
             db.SaveChanges();
         }
     }
