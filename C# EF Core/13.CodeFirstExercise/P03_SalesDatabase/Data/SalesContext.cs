8f59c363ff0f40e1d054dcae9693cb5b24e4b422
zjelev <zlatomir@gmail.com>
2022-07-05 22:14:51 +0300

    Task 3

--- C# EF Core/13.CodeFirstExercise/P03_SalesDatabase/Data/SalesContext.cs
+++ C# EF Core/13.CodeFirstExercise/P03_SalesDatabase/Data/SalesContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
 
 namespace P03_SalesDatabase.Data
 {
@@ -6,22 +7,50 @@ namespace P03_SalesDatabase.Data
     {
         public SalesContext()
         {
-            
+
         }
 
         public SalesContext(DbContextOptions options) : base(options)
         {
-            
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=.;Database=Sales;Integrated Security=true");
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Sales;Integrated Security=true");
             }
             base.OnConfiguring(optionsBuilder);
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsUnicode();
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsUnicode();
+
+                entity.Property(e => e.Email)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreditCardNumber)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsUnicode();
+            });
+
+        }
+
     }
 }
