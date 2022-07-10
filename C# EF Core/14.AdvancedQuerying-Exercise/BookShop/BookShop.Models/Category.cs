bb9e4f43bd3650251e87936a02e2cacae8afa15d
zjelev <zlatomir@gmail.com>
2022-07-10 20:55:01 +0300

    Initial

---
+++ C# EF Core/14.AdvancedQuerying-Exercise/BookShop/BookShop.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        public Category()
+        {
+            this.CategoryBooks = new HashSet<BookCategory>();
+        }
+
+        public int CategoryId { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<BookCategory> CategoryBooks { get; set; }
+    }
+}
