a4c8243207a7fa6d7d11be7ac235ddcdf53605d3
zjelev <zlatomir@gmail.com>
2022-07-15 22:23:51 +0300

    task 3

--- C# EF Core/14.AdvancedQuerying-Exercise/BookShop/BookShop/StartUp.cs
+++ C# EF Core/14.AdvancedQuerying-Exercise/BookShop/BookShop/StartUp.cs
@@ -1,5 +1,10 @@
 ﻿namespace BookShop
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
 
@@ -8,7 +13,54 @@
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            // DbInitializer.ResetDatabase(db);
+
+            // string input = Console.ReadLine();
+
+            string result = GetBooksByPrice(db);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+         {
+            List<string> bookTitles = context.Books
+                .AsEnumerable()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => b.Title)
+                .OrderBy(bt => bt)
+                .ToList();
+
+            return String.Join(Environment.NewLine, bookTitles);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            List<string> bookTitles = context.Books
+                .AsEnumerable()
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title )
+                .ToList();
+
+            return String.Join(Environment.NewLine, bookTitles);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var bookTitlesPrices = context.Books
+                .AsEnumerable()
+                .Where(b => b.Price > 40m)
+                .OrderByDescending(b => b.Price)
+                .Select(b => new { b.Title, b.Price })
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in bookTitlesPrices)
+            {
+                sb.AppendLine($"{item.Title} - ${item.Price}");
+            }
+
+            return sb.ToString();
         }
     }
 }
