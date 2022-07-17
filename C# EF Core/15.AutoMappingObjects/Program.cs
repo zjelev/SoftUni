caab997f84bf9e2a1c346866b51c0098311c3b3d
zjelev <zlatomir@gmail.com>
2022-07-17 22:03:07 +0300

    automapper

--- C# EF Core/15.AutoMappingObjects/Program.cs
+++ C# EF Core/15.AutoMappingObjects/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using AutoMapping.Models;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -11,14 +13,31 @@ namespace AutoMapping
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            var dbContext = new MusicXContext();
-            var service = new ArtistsService(new MusicXContext());
-            var artists = service.GetAllWithCount();
-            Print(artists);
+            var db = new MusicXContext();
+            // var service = new ArtistsService(db);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Artists, ArtistWithCount>();
+            });
+            IMapper mapper = config.CreateMapper();
+
+            var artist = db.Artists
+                // .Include(a => a.SongArtists)
+                .FirstOrDefault();
+
+            var artistViewModel = mapper.Map<ArtistWithCount>(artist);
+
+            // db.Entry(artists).Collection(a => a.SongArtists).Load();
+
+            // var artistWithCount = new ArtistWithCount();
+            // artistWithCount.Name = artists.Name;
+            // artistWithCount.SongsCount = artists.SongArtists.Count();
+
+            Print(artistViewModel);
         }
 
         // View
-        public static void Print(IEnumerable<ArtistWithCount> artists)
+        public static void Print(object artists)
         {
             // foreach (var artist in artists)
             // {
@@ -30,7 +49,8 @@ namespace AutoMapping
         public class ArtistWithCount // DAO DTO
         {
             public string Name { get; set; }
-            public int SongsCount { get; set; }
+            public int SongArtistsCount { get; set; }
+            public DateTime CreatedOn { get; set; }
         }
     }
 }
