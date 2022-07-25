8748a439ee412dba24cd9a29a3ca73bf5125a498
zjelev <zlatomir@gmail.com>
2022-07-25 22:00:30 +0300

    3:05

---
+++ C# Web Basics/AsynchronousProcessing/Tasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task Parallel Library(TPL)
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }).ContinueWith((previousTask) =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    //Task.Run
+            //    new Thread
+            //        (() =>
+            //    {
+            //        Thread.Sleep(100000);
+            //    })
+            //    .Start();
+            //    ;
+            //}
+            Console.WriteLine();
+        }
+    }
+}
