25a70451e701e7f28d71b0c9ba013c0fb4c940fc
zjelev <zlatomir@gmail.com>
2022-07-30 07:46:45 +0300

    State Management complete

--- C# Web Basics/AsynchronousProcessing/HttpRequester/Program.cs
+++ C# Web Basics/AsynchronousProcessing/HttpRequester/Program.cs
@@ -7,6 +7,8 @@ namespace HttpRequester
 {
     class Program
     {
+        static Dictionary<string, int> SessionStore = new Dictionary<string, int>();
+
         static async Task Main(string[] args)
         {
             TcpListener listener = new TcpListener(IPAddress.Loopback, 80);
@@ -23,30 +25,43 @@ namespace HttpRequester
         private static async Task ProcessClientAsync(TcpClient client)
         {
             const string NewLine = "\r\n";
-            NetworkStream stream = client.GetStream();
-            // TODO: Use buffer
-            byte[] requestBytes = new byte[1000000];
+            using NetworkStream stream = client.GetStream();
+            byte[] requestBytes = new byte[1000000];    // TODO: Use buffer
             int bytesRead = await stream.ReadAsync(requestBytes, 0, requestBytes.Length);
             string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+            
             var username = Regex.Match(request, @"Cookie: user=[\w]*").Value;
-            var sid = Regex.Match(request, @"sid=[\n]*\n").Value;
-
+            var sid = Regex.Match(request, @"sid=[^\n]*\n").Value?.Replace("sid=", String.Empty).Trim();
+            var newSid = Guid.NewGuid().ToString();
+            var count = 0;
+            if(SessionStore.ContainsKey(sid))
+            {
+                SessionStore[sid]++;
+                count = SessionStore[sid];
+            }
+            else
+            {
+                sid = null;
+                SessionStore[newSid] = 1;
+                count = 1;
+            }
             string responseText = 
-"<h1>" + username + "</h1>"
+"<h1>" + username + "</h1>" + 
+"<h1>" + count + "</h1>" + 
 //@"<form action='/Account/Login' method='post'> 
 // <input type=date name='date' /> 
 // <input type=text name='username' /> 
 // <input type=password name='password' /> 
 // <input type=submit name='Login' /> 
 // </form>
-+"<h1>" + DateTime.UtcNow + "</h1>";
+"<h1>" + DateTime.UtcNow + "</h1>";
             // Thread.Sleep(1000);
             string response = "HTTP/1.0 200 OK" + NewLine +
                               "Server: MyFirstServer/1.0" + NewLine +
                               "Set-Cookie: lang=bg; Path=/lang" + NewLine +
                               (string.IsNullOrWhiteSpace(sid) ? 
-                                ("Set-Cookie: sid=" + Guid.NewGuid().ToString() + NewLine) 
-                                : String.Empty) +
+                                ("Set-Cookie: sid=" + newSid + NewLine) 
+                                : string.Empty) +
                               // "Set-Cookie: user=Niki; Expires=" + new DateTime(2023, 1, 1).ToString("R") + NewLine +
                               "Set-Cookie: user=Niki; Max-Age=3600; Secure; HttpOnly; SameSite=Strict" + NewLine + 
                               "Content-Type: text/html" + NewLine + //MIME type
@@ -59,17 +74,7 @@ namespace HttpRequester
             await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
             // stream.Flush();
             Console.WriteLine(request);
-            System.Console.WriteLine(new string('=', 60));
-        }
-
-        public static async Task HttpRequest()
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "My Console Browser");
-            var response = await client.GetAsync("https://softuni.bg");
-            string result = await response.Content.ReadAsStringAsync();
-            File.WriteAllText("index.html", result);
+            Console.WriteLine(new string('=', 60));
         }
     }
 }
-
