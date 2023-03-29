2dc231389b873d885a7a4649ce8e9eb5f3a46e65
zjelev <zlatomir@gmail.com>
2023-03-29 07:43:28 +0300

    To move in ASP.NET folder

--- MyFirstMvcApp/Services/IUsersService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyFirstMvcApp.Services;
-
-public interface IUsersService
-{
-    int GetCount();
-    IEnumerable<string> GetUsernames();
-    string LatestUserName(string orderBy = "username");
-}
