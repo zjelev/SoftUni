20f33714643ba07e1e87fab32e1fd17cf8c4ef3d
zjelev <zlatomir@gmail.com>
2023-03-29 08:08:43 +0300

    Add files via upload

---
+++ ASP.NET/MiddlewareDemo/AddInfoHeader.cs
@@ -0,0 +1,18 @@
+﻿public class AddInfoHeader
+{
+    private readonly RequestDelegate next;
+
+    public AddInfoHeader(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        //context.Response.Headers.Add("X-Length", context.Request.ContentLength.ToString());
+        //context.Response.Headers.Add("X-Query", context.Request.QueryString.ToString());
+        context.Response.WriteAsync("Hello from AddInfoHeader!");
+        await next(context);
+    }
+}
+
---
+++ ASP.NET/MiddlewareDemo/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Localization;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDistributedMemoryCache();
+//builder.Services.AddSession();
+var app = builder.Build();
+
+//app.UseWelcomePage();
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Hello", "Hello from my midleware ");
+    context.Items.Add("Username", "Niki");
+    await next();
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("...Before...");
+    if (DateTime.Now.Second % 2 == 0)
+        await next();
+
+    await context.Response.WriteAsync("...After...");
+    await context.Response.WriteAsync(context.Items["Username"].ToString());
+});
+
+app.MapGet("/", (HttpContext http) =>
+{
+    http.Response.WriteAsync("Hello from MapGet/ ");
+    http.Response.WriteAsync(http.Request.Headers["User-agent"]);
+    //http.Session.Set("UserId", new byte[] { 0x0 });
+});
+
+app.Map("/SoftUni", builder =>
+    builder.Run((context) => context.Response.WriteAsync("Hello from SoftUni Map")));
+
+app.UseMiddleware<AddInfoHeader>();
+
+var supportedCultures = new string[] { "bg-BG", "en-US" };
+app.UseRequestLocalization(options =>
+    options.AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures)
+    .SetDefaultCulture("bg-BG")
+    .RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(context =>
+    {
+        return Task.FromResult(new ProviderCultureResult("bg-BG"));
+    }))
+);
+
+// app.UseSession();
+
+app.Run();
