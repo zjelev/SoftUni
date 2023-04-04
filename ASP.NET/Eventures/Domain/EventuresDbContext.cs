b37efcaea5c1f35066020e7db42a32254211f8a4
zjelev <zlatomir@gmail.com>
2023-04-04 11:07:05 +0300

    Add files via upload

---
+++ ASP.NET/Eventures/Domain/EventuresDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain;
+public class EventuresDbContext : IdentityDbContext<EventuresUser, IdentityRole, string>
+{
+    public EventuresDbContext(DbContextOptions<EventuresDbContext> options) : base(options)
+    {
+    }
+}
