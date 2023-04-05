c233dd711e1abcbb93211d6cdb989f6008085469
zjelev <zlatomir@gmail.com>
2023-04-05 09:31:53 +0300

    Identity

--- ASP.NET/Eventures/Eventures/Program.cs
+++ ASP.NET/Eventures/Eventures/Program.cs
@@ -1,9 +1,45 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EventuresDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Eventures")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<EventuresDbContext>();
+
+builder.Services.AddIdentity<EventuresUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 0;
+    // options.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<EventuresDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+using var context = scope.ServiceProvider.GetRequiredService<EventuresDbContext>();
+
+// dotnet ef migrations add Initial -o Migrations
+context.Database.Migrate();
+
+if (!context.Roles.Any())
+{
+    context.Roles.Add(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
+    context.Roles.Add(new IdentityRole { Name = "User", NormalizedName = "USER" });
+}
+
+context.SaveChanges();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
