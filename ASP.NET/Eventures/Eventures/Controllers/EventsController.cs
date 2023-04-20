96586169ad3d8cc11965e637fd7113acb41ff6e9
zjelev <zlatomir@gmail.com>
2023-04-20 16:07:00 +0300

    Add Events Create and Index

--- ASP.NET/Eventures/Eventures/Controllers/EventsController.cs
+++ ASP.NET/Eventures/Eventures/Controllers/EventsController.cs
@@ -1,5 +1,8 @@
 ﻿using Domain;
+using Eventures.Models.BindingModels;
+using Eventures.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace Eventures.Controllers
 {
@@ -13,7 +16,42 @@ namespace Eventures.Controllers
         }
         public IActionResult Index()
         {
+            List<EventsViewModel> events = context.Events
+                .Select(eventFromDb => new EventsViewModel
+                {
+                    Name = eventFromDb.Name,
+                    Place = eventFromDb.Place,
+                    Start = eventFromDb.Start.ToString("dd-MMM-yyyy HH:mm", CultureInfo.InvariantCulture),
+                    End = eventFromDb.End.ToString("dd-MMM-yyyy HH:mm", CultureInfo.InvariantCulture)
+                })
+                .ToList();
+
+            return View(events);
+        }
+
+        public IActionResult Create(EventCreateBindingModel bindingModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                Event eventForDb = new Event
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = bindingModel.Name,
+                    Place = bindingModel.Place,
+                    Start = bindingModel.Start,
+                    End = bindingModel.End,
+                    TotalTickets = bindingModel.TotalTickets,
+                    PricePerTicket = bindingModel.PricePerTicket
+                };
+
+                context.Events.Add(eventForDb);
+                context.SaveChanges();
+
+                return this.RedirectToAction("Index");
+            }
+
             return View();
+
         }
     }
 }
