77cb364f86aa18eb04fe6ba26be57b0623861b15
zjelev <zlatomir@gmail.com>
2023-05-02 19:00:43 +0300

    appendChild

--- JSAdvanced/04-DOM-Lab/01. Articles List/solution.js
+++ JSAdvanced/04-DOM-Lab/01. Articles List/solution.js
@@ -1,4 +1,22 @@
 function createArticle() {
+	let titleInputElement = document.getElementById('createTitle');
+	let contentInputElement = document.getElementById('createContent');
+	// console.log(titleElement.value);
+	// console.log(createContentElement.value);
+
+	let headingElement = document.createElement("h3");
+	headingElement.innerHTML = titleInputElement.value;
+	titleInputElement.value = '';
+
+	let contentElement = document.createElement("p");
+	contentElement.textContent = contentInputElement.value;
+	contentInputElement.value = '';
+	console.log(contentInputElement.getAttribute('id'));
 	
-	//TODO...
+	let articleElement = document.createElement("article");
+	articleElement.appendChild(headingElement);
+	articleElement.appendChild(contentElement);
+
+	let articleSectionElement = document.getElementById('articles');
+	articleSectionElement.appendChild(articleElement);
 }
