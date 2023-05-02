12b054059e03941d5bd055fb2058d795b56bb9df
zjelev <zlatomir@gmail.com>
2023-05-02 15:55:08 +0300

    DOM Lection complete

--- JSAdvanced/04-DOM-Lab/03. Growing Word/solution.js
+++ JSAdvanced/04-DOM-Lab/03. Growing Word/solution.js
@@ -1,4 +1,18 @@
 function growingWord() {
+  let heading = document.querySelector('h1');
+  // let colors = document.getElementById('colors');
 
-  //TODO...
+  if (!heading.style.fontSize) {
+    let firstColorElement = document.getElementById('blueDiv');
+    firstColorElement.setAttribute('current-color', true);
+    heading.style.fontSize = '2px';
+  } else {
+    let size = parseInt(heading.style.fontSize);
+    heading.style.fontSize = `${size + 2}px`;
+
+    let colorElements = document.querySelector('#colors');
+    for (const element of colorElements.children) {
+      element.style.color = 'blue';  
+     };
+  }
 }
